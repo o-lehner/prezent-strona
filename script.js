dac6d4479f7c462c9c0aad93fc485cb64db675e6
o-lehner <olivierlehner1@gmail.com>
2026-04-22 14:25:59 +0200

    Powiększenie odtwarzacza i ustawienie warstw tak, aby kwiatki nachodziły na ramkę, ale nie na kontrolki

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const pauseIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="6" y="4" width="4" height="16"></rect><rect x="14" y="4" width="4" height="16"></rect></svg>';
 
     // --- Flower Generation (Better Anti-Overlap) ---
-    const numberOfFlowers = 22;
+    const numberOfFlowers = 30;
     const placedFlowers = [];
     const flowerSize = 80; // Rozmiar kwiatka w px z CSS
     const padding = 10; // Minimalny odstęp między kwiatkami w px
